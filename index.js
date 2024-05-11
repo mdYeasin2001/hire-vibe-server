25d24282ab4f4a47ed809ffd66c5cca8929bef74
Md. Maynul Islam <islammaynul82@gmail.com>
2024-05-11 16:51:07 +0600

    added get func to get jobs

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 9000;
 
 const app = express()
 
 //middleware
+
+// jobHive
+// pOcjwCYriRgCDdAy
 
 const corsOptions = {
     origin: ['http://localhost:5173'],
@@ -14,6 +18,44 @@ const corsOptions = {
 }
 app.use(cors({corsOptions}))
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4wc44xb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+      
+     const jobsCollection  = client.db('jobHive').collection('jobs')   
+     const appliedJobsCollection  = client.db('jobHive').collection('appliedJobs')   
+
+
+    //get all jobs data from mongodb
+    app.get('/jobs',async(req,res)=>{
+        const result = await jobsCollection.find().toArray()
+        res.send(result)
+    })
+
+
+      // Send a ping to confirm a successful connection
+    //   await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+
 
 app.get('/',(req,res)=>[
     res.send('Job Hive server is running...')
